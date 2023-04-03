853c358d7478f3b9d55a83fa45824771755c5def
Tanvi Reddy Vuyyuru <tanvireddyvuyyuru@Tanvis-MacBook-Pro.local>
2023-04-03 06:37:14 -0400

    commit4

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,23 +1,23 @@
 export default [
   {
     "_id": "123", "topic": "Space", "username": "SpaceX",
-    "handle": "@spacex", "time": "2h", "image": "https://pbs.twimg.com/media/EzpaM-JXEAAJwRQ.jpg",
+    "handle": "spacex", "time": "2h", "image": "https://pbs.twimg.com/media/EzpaM-JXEAAJwRQ.jpg",
     "title": "SpaceX's Mission",
     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-    "liked": true, "likes": 2345, "dislikes": 1264,"replies": 123, "retuits": 432
+    "liked": true, "likes": 2345, "dislikes": 124,"replies": 123, "retuits": 432
   },
   {
     "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-    "handle": "@boringcompany", "time": "2h", "image": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1493657922/ceit0qzp8rhqucbcaox6.png",
+    "handle": "boringcompany", "time": "2h", "image": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1493657922/ceit0qzp8rhqucbcaox6.png",
     "title": "The Boring Company fixing traffic",
     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-    "liked": true, "likes": 2345, "dislikes": 1264,"replies": 321, "retuits": 432
+    "liked": true, "likes": 2345, "dislikes": 148,"replies": 321, "retuits": 432
   },
   {
     "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-    "handle": "@neurolink", "time": "3h", "image": "https://pbs.twimg.com/profile_images/895407294636187648/xoppJ-eR_400x400.jpg",
+    "handle": "neurolink", "time": "3h", "image": "https://pbs.twimg.com/profile_images/895407294636187648/xoppJ-eR_400x400.jpg",
     "title": "Neurolink fixes paralisis and blindness",
     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-    "liked": true, "likes": 3456, "dislikes": 1264, "replies": 345, "retuits": 654
+    "liked": true, "likes": 3456, "dislikes": 152, "replies": 345, "retuits": 654
   },
 ];
