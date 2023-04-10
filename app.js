925911b04ef576cbcc0fa7e70dd3481a5dffd166
Tanvi Reddy Vuyyuru <tanvireddyvuyyuru@Tanvis-MacBook-Pro.local>
2023-04-10 02:25:59 -0400

    a9 Mongo spring23

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 //mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
-const DB_CONNECTION_STRING='mongodb+srv://tanvireddyvuyyuru:<supersecretpassword>@cluster0.ponswo4.mongodb.net/?retryWrites=true&w=majority'
-const CONNECTION_STRING = DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING||'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express()
