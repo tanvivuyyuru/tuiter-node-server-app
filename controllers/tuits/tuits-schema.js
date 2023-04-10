b4beeca4efea6e1c18b85e47cf617d1ad14c54d4
Tanvi Reddy Vuyyuru <tanvireddyvuyyuru@Tanvis-MacBook-Pro.local>
2023-04-10 01:07:21 -0400

    a9 Mongo spring23

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,17 +1,8 @@
 import mongoose from 'mongoose';   // load the mongoose library
 const schema = mongoose.Schema({   // create the schema
-        topic: String,
-        username:String,
-        handle:String,
-        time:String,
-        image:String,
-        title:String,
-        tuit: String,
-        likes: Number,
-        liked: Boolean,
-        dislikes: Number,
-        replies: Number,
-        retuits: Number
+            tuit: String,
+            likes: Number,
+            liked: Boolean,
     },
     {collection: 'tuits'});  // collection name where tuits are stored in tuiter database
 
